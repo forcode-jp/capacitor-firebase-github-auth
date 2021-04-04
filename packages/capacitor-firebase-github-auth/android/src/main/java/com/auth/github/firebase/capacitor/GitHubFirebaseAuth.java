9027d0a250c494c53100b58c0904a91ef5f5f443
Masato Urai <masato.uraway@gmail.com>
2021-04-05 03:14:44 +0900

    android

--- packages/capacitor-firebase-github-auth/android/src/main/java/com/auth/github/firebase/capacitor/GitHubFirebaseAuth.java
+++ packages/capacitor-firebase-github-auth/android/src/main/java/com/auth/github/firebase/capacitor/GitHubFirebaseAuth.java
@@ -2,18 +2,34 @@ package com.auth.github.firebase.capacitor;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.OAuthCredential;
+import com.google.firebase.auth.OAuthProvider;
 
 @NativePlugin
-public class GitHubFirebaseAuth extends Plugin {
+public class GitHubFirebaseAuth extends Plugin implements OnSuccessListener<AuthResult>, OnFailureListener {
+    private static final String TAG = "GitHubFirebaseAuth";
+
+    private FirebaseAuth firebaseAuth;
+    private OAuthProvider.Builder provider;
+    private AuthCredential authCredential;
 
     public void load() {
-        // Called when the plugin is first constructed in the bridge
-        Log.d("GitHubFirebaseAuth: Initialized");
+        Log.i(TAG, "initialized");
+        this.firebaseAuth = FirebaseAuth.getInstance();
+        this.provider = OAuthProvider.newBuilder("github.com");
     }
 
     @PluginMethod
@@ -27,7 +43,7 @@ public class GitHubFirebaseAuth extends Plugin {
 
     @PluginMethod
     public void signIn(PluginCall call) {
-        OAuthProvider.Builder provider = OAuthProvider.newBuilder("github.com");
+        this.saveCall(call);
 
         Task<AuthResult> pendingResultTask = firebaseAuth.getPendingAuthResult();
         if (pendingResultTask != null) {
@@ -36,23 +52,37 @@ public class GitHubFirebaseAuth extends Plugin {
                 .addOnSuccessListener(this)
                 .addOnFailureListener(this);
         } else {
+            // There's no pending result so you need to start the sign-in flow.
+            // See below.
             firebaseAuth
-                .startActivityForSignInWithProvider(/* activity= */ this, provider.build())
+                .startActivityForSignInWithProvider(/* activity= */ this.bridge.getActivity(), provider.build())
                 .addOnSuccessListener(this)
-                .addOapnFailureListener(this);
+                .addOnFailureListener(this);
         }
     }
 
     @PluginMethod
-    public  void signOut(PluginCall call) {}
-
-    @Override
-    public void onSuccess(AuthResult authResult) {
-        // Handle Success
+    public void signOut(PluginCall call) {
+        this.firebaseAuth.getInstance().signOut();
     }
 
     @Override
     public void onFailure(@NonNull Exception e) {
-        // Handle Failure
+        Log.w(TAG, e);
+        PluginCall call = getSavedCall();
+
+        call.reject(e.getLocalizedMessage());
+    }
+
+    @Override
+    public void onSuccess(AuthResult authResult) {
+        Log.i(TAG, "successfully signIn");
+        PluginCall call = getSavedCall();
+
+        JSObject result = new JSObject();
+        OAuthCredential authCredential = (OAuthCredential) authResult.getCredential();
+        result.put("accessToken", authCredential.getAccessToken());
+
+        call.success(result);
     }
 }
