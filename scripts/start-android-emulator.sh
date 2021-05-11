cb85667be79286eb686969b3f9a184ef0aa7cf12
Masato Urai <masato.uraway@gmail.com>
2021-05-11 10:44:15 +0900

    -wipe-data

--- scripts/start-android-emulator.sh
+++ scripts/start-android-emulator.sh
@@ -11,7 +11,7 @@ echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n "${AVD_NAME}" -k "s
 echo "Starting emulator"
 
 # Start emulator in background
-nohup $ANDROID_HOME/emulator/emulator -avd ${AVD_NAME} -no-snapshot -noaudio > /dev/null 2>&1 &
+nohup $ANDROID_HOME/emulator/emulator -avd ${AVD_NAME} -no-snapshot -noaudio -wipe-data > /dev/null 2>&1 &
 $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
 
 $ANDROID_HOME/platform-tools/adb devices
